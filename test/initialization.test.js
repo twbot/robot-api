b8cc30f0d5ac3b834d9dd669b6768632f4202974
Ramon Wijnands <rayman747@hotmail.com>
2015-11-18 11:36:58 +0100

    test: add module initialization tests with mocha, chai and sinon

---
+++ test/initialization.test.js
@@ -0,0 +1,69 @@
+/* eslint no-unused-expressions: 0 */
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+chai.should();
+chai.use(sinonChai);
+
+import {stub} from 'sinon';
+
+import Base from '../lib/base';
+import Ed from '../lib/ed';
+import Hardware from '../lib/hardware';
+import Head from '../lib/head';
+import Robot from '../lib/robot';
+
+import {Ros} from 'roslib';
+
+const setup = () => {
+  const ros = new Ros({
+    encoding: 'ascii'
+  });
+
+  const robot = {
+    ros
+  };
+
+  const fixtures = {
+    ros,
+    robot
+  };
+
+  return fixtures;
+};
+
+describe('Module initialization', () => {
+  let fixtures;
+
+  beforeEach('stub callOnConnection', () => {
+    fixtures = setup();
+    const callOnConnection = stub(fixtures.ros, 'callOnConnection');
+    callOnConnection.throws();
+  });
+
+  afterEach('restore callOnConnection', () => {
+    fixtures.ros.callOnConnection.restore();
+  });
+
+  // TODO: remove skipped modules
+  const skipModules = [Hardware, Head];
+
+  [Base, Ed, Hardware, Head].forEach(Module => {
+    describe(`${Module.name} initialization`, () => {
+      const it2 = skipModules.some(key => key === Module) ? it.skip : it;
+
+      it2('should do nothing on creation', () => {
+        const m = new Module(fixtures.robot);
+
+        m.should.be.ok;
+        fixtures.ros.callOnConnection.should.not.have.been.called;
+      });
+    });
+  });
+});
+
+describe('Robot initialization', () => {
+  it('should not crash', () => {
+    const robot = new Robot();
+    robot.should.be.ok;
+  });
+});
