b2f7c35f7fc98a1bb54fcd85639a92ab2f30c7f6
Ramon Wijnands <rayman747@hotmail.com>
2015-11-25 12:35:08 +0100

    test(actionServer): add actionServer.doAction test

---
+++ test/action-server.test.js
@@ -0,0 +1,61 @@
+/* eslint no-unused-expressions: 0 */
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+chai.should();
+
+import {stub} from 'sinon';
+
+import {ActionServer} from '../lib';
+
+import {Ros} from 'roslib';
+
+const setup = () => {
+  const ros = new Ros({
+    encoding: 'ascii'
+  });
+
+  const robot = {
+    ros
+  };
+
+  const fixtures = {
+    ros,
+    robot
+  };
+
+  return fixtures;
+};
+
+describe('ActionServer', () => {
+  let fixtures;
+  let callOnConnection;
+
+  let actionServer;
+
+  before('stub callOnConnection', () => {
+    fixtures = setup();
+    callOnConnection = stub(fixtures.ros, 'callOnConnection');
+    callOnConnection.returns();
+
+    actionServer = new ActionServer(fixtures.robot);
+  });
+
+  after('restore callOnConnection', () => {
+    callOnConnection.restore();
+  });
+
+  describe('ActionServer.doAction', () => {
+    it('should send a service call', () => {
+      actionServer.doAction('pick-up', 'chair');
+
+      const expected = {action: 'pick-up', parameters: '{"entity":{"id":"chair"}}'};
+
+      callOnConnection.should.have.been.calledOnce;
+      const message = callOnConnection.firstCall.args[0];
+      message.op.should.equal('call_service');
+      message.service.should.equal('action_server/add_action');
+      message.args.should.deep.equal(expected);
+    });
+  });
+});
