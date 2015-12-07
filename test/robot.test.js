aac6940aaebd8a03c3c8eb946046d07b468a1b12
Ramon Wijnands <rayman747@hotmail.com>
2015-12-07 14:51:21 +0100

    test(robot): add Robot.status and reconnection tests

--- test/robot.test.js
+++ test/robot.test.js
@@ -2,20 +2,22 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
-chai.should();
+const should = chai.should();
 
-import {stub} from 'sinon';
+import {stub, useFakeTimers} from 'sinon';
 
 import {Robot} from '../lib';
 
 describe('Robot', () => {
-  let connect;
-  let robot;
-
   const exampleUrl = 'ws://example.com:9090';
 
-  beforeEach('stub connect', () => {
+  let robot;
+  beforeEach('create a robot', () => {
     robot = new Robot();
+  });
+
+  let connect;
+  beforeEach('stub connect', () => {
     connect = stub(robot.ros, 'connect');
     connect.returns();
   });
@@ -47,8 +49,78 @@ describe('Robot', () => {
 
       robot.connect();
       connect.should.have.been.calledTwice;
+      connect.should.always.have.been.calledWithExactly(exampleUrl);
+    });
+  });
 
-      connect.should.always.have.been.calledWith(exampleUrl);
+  describe('Robot.status', () => {
+    /**
+     * Stubs
+     */
+    let send;
+    beforeEach('stub ros.socket.send', () => {
+      // stub callOnConnection to prevent sending something on the websocket
+      should.not.exist(null);
+      should.not.exist(robot.ros.socket);
+
+      send = stub();
+      send.returns();
+      robot.ros.socket = {
+        send
+      };
+    });
+
+    afterEach('stub callOnConnection', () => {
+      robot.ros.socket = null;
+    });
+
+    let clock;
+    beforeEach('stub setTimeout', () => {
+      clock = useFakeTimers();
+    });
+
+    afterEach('restore setTimeout', () => {
+      clock.restore();
+    });
+
+    /**
+     * Tests
+     */
+    it.skip('should have a default status', () => {
+      robot.status.should.equal('closed');
+    });
+
+    it('should respond to the ros::connection event', () => {
+      robot.connect(exampleUrl);
+      robot.status.should.equal('connecting');
+      connect.should.have.been.calledOnce;
+      connect.should.have.been.calledWithExactly(exampleUrl);
+
+      robot.ros.emit('connection');
+      robot.status.should.equal('connected');
+    });
+
+    it('should reconnect after the ros::closed event', () => {
+      robot.connect(exampleUrl);
+      connect.should.have.been.calledOnce;
+      connect.should.have.been.calledWithExactly(exampleUrl);
+
+      robot.ros.emit('close');
+      robot.status.should.equal('closed');
+      clock.tick(500);
+
+      connect.should.have.been.calledOnce;
+      connect.should.have.been.calledWithExactly(exampleUrl);
+
+      clock.tick(5000);
+
+      connect.should.have.been.calledTwice;
+      connect.should.always.have.been.calledWithExactly(exampleUrl);
+    });
+
+    it('should respond to the ros::error event', () => {
+      robot.ros.emit('error');
+      robot.status.should.equal('error');
     });
   });
 });
